e236ee69777a429335e2d4a536e159f5caf36f33
Alex Rodriguez <rodriguez.al183@gmail.com>
2026-01-21 19:07:27 -0800

    feat(scripts): add hacknet auto-upgrader

---
+++ bitburner/scripts/hacknet-auto.js
@@ -0,0 +1,34 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+  ns.disableLog("ALL");
+
+  const maxLevel = 200;
+  const maxRam = 64;
+  const maxCores = 16;
+
+  while (true) {
+    while (ns.hacknet.purchaseNode() !== -1) {}
+
+    const count = ns.hacknet.numNodes();
+    for (let i = 0; i < count; i++) {
+      const stats = ns.hacknet.getNodeStats(i);
+
+      while (stats.level < maxLevel && ns.hacknet.getLevelUpgradeCost(i, 1) <= ns.getPlayer().money) {
+        if (!ns.hacknet.upgradeLevel(i, 1)) break;
+        stats.level += 1;
+      }
+
+      while (stats.ram < maxRam && ns.hacknet.getRamUpgradeCost(i, 1) <= ns.getPlayer().money) {
+        if (!ns.hacknet.upgradeRam(i, 1)) break;
+        stats.ram *= 2;
+      }
+
+      while (stats.cores < maxCores && ns.hacknet.getCoreUpgradeCost(i, 1) <= ns.getPlayer().money) {
+        if (!ns.hacknet.upgradeCore(i, 1)) break;
+        stats.cores += 1;
+      }
+    }
+
+    await ns.sleep(1000);
+  }
+}
